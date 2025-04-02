a09bd6be2877281b42b515c8e1454bdd5920e07d
natan <cesardasilvanataniel@gmail.com>
2025-04-02 18:31:12 -0300

    Add CRUD on FamilyController

--- src/main/java/com/splithome/application/controllers/FamilyController.java
+++ src/main/java/com/splithome/application/controllers/FamilyController.java
@@ -1,4 +1,51 @@
 package com.splithome.application.controllers;
 
+
+import com.splithome.application.entities.Family;
+import com.splithome.application.services.FamilyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/family")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FamilyController {
+
+    @Autowired
+    private FamilyService familyService;
+
+    @PostMapping("new-family")
+    public ResponseEntity<Family> createFamily(@RequestBody Family family) {
+        familyService.saveFamily();
+        return ResponseEntity.ok(family);
+    }
+
+    @GetMapping
+    public ResponseEntity<Family> getFamilyById(@PathVariable UUID id) {
+        Family family = familyService.getFamilyById(id);
+        return ResponseEntity.ok(family);
+    }
+
+    @GetMapping("families")
+    public ResponseEntity<List<Family>> getAllFamily() {
+        List<Family> families = familyService.getAllFamily();
+        return ResponseEntity.ok(families);
+    }
+
+    @PutMapping
+    public ResponseEntity<Family> updateFamily(@RequestBody Family family) {
+        Family newFamily = familyService.updateFamily(family);
+        return ResponseEntity.ok(newFamily);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteFamily(@PathVariable UUID id) {
+        familyService.removeFamily(id);
+        return ResponseEntity.ok("family deleted");
+    }
+
 }
