1d3cec84d41b06d11995552cda14448fbb78a336
natan <cesardasilvanataniel@gmail.com>
2025-04-02 22:09:46 -0300

    add family field in getUserById and getAllUsers

--- src/main/java/com/splithome/application/services/UserService.java
+++ src/main/java/com/splithome/application/services/UserService.java
@@ -68,14 +68,14 @@ public class UserService {
 
     public UserDTO getUserById(UUID id) {
         User user = userRepository.getUsersById(id);
-        return new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getPhoneNumber(), user.getPixKey());
+        return new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getPhoneNumber(), user.getPixKey(), user.getFamily());
     }
 
     public List<UserDTO> getAllUsers() {
         List<User> users = userRepository.findAll();
         List<UserDTO> usersDto = new ArrayList<>();
         for (User user : users) {
-            UserDTO userDto = new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getPhoneNumber(), user.getPixKey());
+            UserDTO userDto = new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getPhoneNumber(), user.getPixKey(), user.getFamily());
             usersDto.add(userDto);
         }
         return usersDto;
