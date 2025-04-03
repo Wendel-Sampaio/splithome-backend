7f1f41911bfd673e1f49b4e131ade9060dc8d19a
natan <cesardasilvanataniel@gmail.com>
2025-04-02 22:09:15 -0300

    add JsonBackReference

--- src/main/java/com/splithome/application/entities/User.java
+++ src/main/java/com/splithome/application/entities/User.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
@@ -52,6 +53,7 @@ public class User implements UserDetails {
 
     @ManyToOne
     @JoinColumn(name = "family_id")
+    @JsonBackReference
     private Family family;
 
     public User(String name, String email, String encryptedPassword, String phoneNumber, String pixKey, Family family) {
