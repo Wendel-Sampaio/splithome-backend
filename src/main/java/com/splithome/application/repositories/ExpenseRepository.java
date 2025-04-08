a5762bbe1efc25951575b79113627eb7b4fdb41c
natan <cesardasilvanataniel@gmail.com>
2025-04-08 11:28:04 -0300

    Refactor to set family and filter by family in TransactionService methods

--- src/main/java/com/splithome/application/repositories/ExpenseRepository.java
+++ src/main/java/com/splithome/application/repositories/ExpenseRepository.java
@@ -4,8 +4,10 @@ import com.splithome.application.entities.transaction.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
+    List<Expense> findAllByFamilyCode(String familyCode);
 }
--- src/main/java/com/splithome/application/services/TransactionService.java
+++ src/main/java/com/splithome/application/services/TransactionService.java
@@ -1,13 +1,19 @@
 package com.splithome.application.services;
 
+import com.splithome.application.entities.Family;
+import com.splithome.application.entities.User;
 import com.splithome.application.entities.transaction.Expense;
 import com.splithome.application.entities.transaction.Purchase;
+import com.splithome.application.exceptions.FamilyNotFoundException;
 import com.splithome.application.exceptions.PurchaseNotFoundException;
 import com.splithome.application.exceptions.PurchaserNotFoundException;
 import com.splithome.application.repositories.ExpenseRepository;
+import com.splithome.application.repositories.FamilyRepository;
 import com.splithome.application.repositories.PurchaseRepository;
 import com.splithome.application.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,24 +32,30 @@ public class TransactionService {
     @Autowired
     private ExpenseRepository expenseRepository;
 
+    @Autowired
+    FamilyRepository familyRepository;
+
     public List<Purchase> getAllPurchases() {
-        return purchaseRepository.findAll();
+        return purchaseRepository.findAllByFamilyFamilyCode(this.getUserFamilyCode());
     }
 
     public List<Expense> getAllExpenses() {
-        return expenseRepository.findAll();
+        return expenseRepository.findAllByFamilyCode(this.getUserFamilyCode());
     }
 
     public void savePurchase(Purchase purchase) {
         if (userRepository.existsById(purchase.getPurchaserId())) {
+            purchase.setFamily(familyRepository.getFamilyByFamilyCode(this.getUserFamilyCode()));
             purchaseRepository.save(purchase);
         } else {
             throw new PurchaserNotFoundException();
         }
     }
 
+
     public void saveExpense(Expense expense) {
         if (userRepository.existsById(expense.getResponsibleId())) {
+            expense.setFamily(familyRepository.getFamilyByFamilyCode(this.getUserFamilyCode()));
             expenseRepository.save(expense);
         }
     }
@@ -66,4 +78,14 @@ public class TransactionService {
             throw new PurchaseNotFoundException();
         }
     }
+
+    private String getUserFamilyCode(){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails userDetails) {
+            User user = (User) userRepository.findByEmail(userDetails.getUsername());
+           return user.getFamily().getFamilyCode();
+        }
+        throw new FamilyNotFoundException();
+    }
+
 }
